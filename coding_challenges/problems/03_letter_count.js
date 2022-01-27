2c1990c07bb8af8093c027e0855a2c205eb551f1
lightme-fan <fanilo.and@onja.org>
2022-01-27 17:04:08 +0300

    Solution: resolved letter-count

--- coding_challenges/problems/03_letter_count.js
+++ coding_challenges/problems/03_letter_count.js
@@ -3,7 +3,24 @@
 // spaces.
 
 function letterCount(str) {
+    // Split to get an array of strings
+    let string = str.toLowerCase().split("")
 
+    // Filter out all empty strings
+    let newStr = string.filter(s => s !== " ")
+
+    // Create an empty object
+    let obj = {}
+
+    // Loop through the string array
+    for (let i = 0; i < newStr.length; i++) {
+        // Check if the element is NaN
+        obj[newStr[i]] = (isNaN(obj[newStr[i]]) ? 1 : obj[newStr[i]] + 1);
+        
+    }
+
+    // return the new object
+    return obj
 }
 
 module.exports = letterCount;
