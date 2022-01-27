d7ee4fe911a3928e0413d66e94df889b9912fac8
lightme-fan <fanilo.and@onja.org>
2022-01-27 14:20:09 +0300

    Resolved no-repeat

--- coding_challenges/problems/02_no_repeats.js
+++ coding_challenges/problems/02_no_repeats.js
@@ -5,8 +5,21 @@
 // Hint: It may be helpful to write a helper function `noRepeat(year)`
 // which returns true/false if a single year doesn't have a repeat.
 
-function noRepeats(yearStart, yearEnd) {
+function uniqueNumber(numbers) {
+    return numbers.filter((value, index, self) => self.indexOf(value) === index)
+}
 
+function noRepeats(yearStart, yearEnd) {
+    let noRepeatDigits = []
+
+    for (let i = yearStart; i < yearEnd; i++) {
+         let year = String(i).split("").map(num => Number(num))
+
+        if (uniqueNumber(year).length === year.length) {
+            noRepeatDigits.push(Number(uniqueNumber(year).join("")));
+        }
+    }
+    return noRepeatDigits
 }
 
 module.exports = noRepeats;
